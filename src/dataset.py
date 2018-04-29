2e6b4d2103cf80b0ae6124b539665816266832d7
Koutetsu <Anes.Benmerzoug@gmail.com>
2018-04-30 00:38:12 +0200

    Finished the dataset class. Now it preprocesses it and pickles it for future usage

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,10 @@
-import os
-import string
-import torch
+from __future__ import print_function
 from torch.utils.data import Dataset
+import xml.etree.ElementTree as ET
+import numpy as np
+import pickle
+import string
+import os
 
 
 class IAMDataset(Dataset):
@@ -15,13 +18,100 @@ class IAMDataset(Dataset):
         """
         self.params = parameters
         self.setType = setType.lower()
-        self.data_path_list = []
-        self.create_data_path_list()
+        self.data_filename = os.path.join(self.params.DatasetDir, self.setType + '_data.pickled')
+        self.alphabet = []  # Initially empty
+        self.length = 0
+
+        if not (os.path.exists(self.data_filename)):
+            print("Creating file {}".format(self.data_filename))
+            self.prepocess_data()
+        else:
+            print("File {} exists already".format(self.data_filename))
+
+        self.load_data()
+
+        raise KeyboardInterrupt
+
+    def get_alphabet(self):
+        with open(os.path.join(self.params.DatasetDir, 'task1', 'letters')) as alphabet:
+            for letter in alphabet:
+                self.alphabet.append(letter.replace('\n', ''))
+
+    def prepocess_data(self):
+        data_path_list = self.create_data_path_list()
+        text_array = []
+        strokes_array = []
+
+        def getAscii(filename):
+            with open(filename, "r") as f:
+                text = f.read()
+            text = text[text.find('CSR:')+6:]
+            for c in text:
+                if c not in self.alphabet and c != '\n':
+                    self.alphabet.append(c)
+            return text.split('\n')
+
+        def getStrokes(filename_list):
+            result = []
+            for stroke_file in filename_list:
+                root = ET.parse(stroke_file).getroot()
+                x_offset = min([float(root[0][i].attrib['x']) for i in range(1, 4)])
+                y_offset = min([float(root[0][i].attrib['y']) for i in range(1, 4)])
+                strokes = []
+                for stroke in root[1].findall('Stroke'):
+                    points = [(0.0, 0.0)]
+                    for point in stroke.findall('Point'):
+                        points.append((float(point.attrib['x']) - x_offset,
+                                       float(point.attrib['y']) - y_offset))
+                    strokes.append(points)
+                result.append(strokes)
+            return result
+
+        def convert_stroke_to_array(stroke):
+            n_point = 0
+            for i in range(len(stroke)):
+                n_point += len(stroke[i])
+            stroke_data = np.zeros((n_point, 3), dtype=np.int16)
+
+            prev_x = 0
+            prev_y = 0
+            counter = 0
+
+            for j in range(len(stroke)):
+                for k in range(len(stroke[j])):
+                    stroke_data[counter, 0] = int(stroke[j][k][0]) - prev_x
+                    stroke_data[counter, 1] = int(stroke[j][k][1]) - prev_y
+                    prev_x = int(stroke[j][k][0])
+                    prev_y = int(stroke[j][k][1])
+                    stroke_data[counter, 2] = 0
+                    if k == (len(stroke[j]) - 1):  # end of stroke
+                        stroke_data[counter, 2] = 1
+                    counter += 1
+            return stroke_data
+
+        for ascii_file, strokes_files in data_path_list:
+            # Get the text from the files
+            text_list = getAscii(ascii_file)
+
+            # Get the strokes from the files
+            strokes_list = getStrokes(strokes_files)
+
+            for text, strokes in zip(text_list, strokes_list):
+                if len(text) > 10:
+                    text_array.append(text)
+                    strokes_array.append(convert_stroke_to_array(strokes))
+                else:
+                    print("\nText was too short: {}".format(text))
+
+        assert(len(text_array) == len(strokes_array))
+        with open(self.data_filename, 'wb+') as f:
+            pickle.dump([text_array, strokes_array], f)
 
     def create_data_path_list(self):
         type_filename = 'trainset.txt' if self.setType == 'train' else \
                         'testset_v.txt' if self.setType == 'validate' else \
                         'testset_f.txt'
+        data_path_list = []
         for filename in os.listdir(os.path.join(self.params.DatasetDir, 'task1')):
             if filename == type_filename:
                 with open(os.path.join(self.params.DatasetDir, 'task1', filename)) as setTextFile:
@@ -44,10 +134,16 @@ class IAMDataset(Dataset):
                             if line in stroke:
                                 strokes.append(os.path.join(stroke_path, stroke))
                         strokes.sort(key=lambda name: int(name[-6:-4]))
-                        self.data_path_list.append((ascii_path, strokes))
+                        data_path_list.append((ascii_path, strokes))
+        return data_path_list
+
+    def load_data(self):
+        with open(self.data_filename, 'rb') as f:
+            self.ascii, self.strokes = pickle.load(f)
+        print(self.strokes)
 
     def __len__(self):
-        return len(self.data_path_list)
+        return self.length
 
     def __getitem__(self, idx):
         ascii_path, strokes_paths = self.data_path_list[idx]
