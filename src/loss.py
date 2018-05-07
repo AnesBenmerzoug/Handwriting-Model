Corrected a mistake in the loss computation that made the model diverge and added a few other things

src/loss.py
@@ -15,18 +15,15 @@ class HandwritingLoss(Module):
         y_data = stroke[0, 0, 1]
         eos_data = stroke[0, 0, 2]
         N = self.bivariateGaussian(x_data, y_data, mu1, mu2, sigma1, sigma2, rho)
-        Pr = pi * N
-        Pr = torch.clamp(Pr, 1e-20, torch.max(Pr).data[0])
-        loss = - torch.log(Pr) \
-               - torch.log(eos * eos_data + (1 - eos) * (1 - eos_data))
-        return torch.sum(loss)
+        Pr = torch.sum(pi * N)
+        loss = - torch.log(Pr + 1e-20) \
+               - torch.log(eos * eos_data + (1.0 - eos) * (1.0 - eos_data))
+        return loss.view(1)
 
     def bivariateGaussian(self, x, y, mu1, mu2, sigma1, sigma2, rho):
-        x_mu1 = x - mu1
-        y_mu2 = y - mu2
-        Z = torch.pow(x_mu1, 2)/sigma1 \
-            + torch.pow(y_mu2, 2)/sigma2 \
-            - 2 * rho * x_mu1 * y_mu2 / (sigma1 * sigma2)
-        N = torch.exp(-Z / (2*(1 - torch.pow(rho, 2)))) \
-            / (2 * np.pi * sigma1 * sigma2 * torch.sqrt(1 - torch.pow(rho, 2)))
+        Z = ((x - mu1) / sigma1) ** 2.0 \
+            + ((y - mu2) / sigma2) ** 2.0 \
+            - 2.0 * rho * (x - mu1) * (y - mu2) / (sigma1 * sigma2)
+        N = torch.exp(- 0.5 * Z / (1.0 - rho ** 2.0)) \
+            / (2.0 * np.pi * sigma1 * sigma2 * torch.sqrt(1.0 - rho ** 2.0))
         return N

src/modules.py
@@ -13,15 +13,13 @@ class GaussianWindow(Module):
     def forward(self, input_, onehot, prev_kappa=None):
         abk_hats = self.parameter_layer(input_)
         abk = torch.exp(abk_hats)
-        alpha, beta, kappa = torch.chunk(abk, 3, dim=2)
-        if prev_kappa is None:
-            prev_kappa = kappa
-        else:
-            kappa, prev_kappa = kappa + prev_kappa, kappa
+        alpha, beta, kappa = abk.chunk(3, dim=2)
+        if prev_kappa is not None:
+            kappa = kappa + prev_kappa
         u = torch.autograd.Variable(torch.arange(0, onehot.size(1)).view(-1, 1).expand(-1, kappa.size(2)))
-        phi = torch.sum(alpha * torch.exp(-beta * torch.pow(kappa - u, 2)), dim=2).view(1, 1, -1)
+        phi = torch.sum(alpha * torch.exp(-beta * ((kappa - u) ** 2)), dim=2).view(1, -1)
         window = torch.matmul(phi, onehot)
-        return window, prev_kappa
+        return window, kappa
 
     def __repr__(self):
         s = '{name}(input_size={input_size}, num_components={num_components})'
@@ -38,9 +36,11 @@ class MDN(Module):
     def forward(self, input_):
         parameters_hats = self.parameter_layer(input_)
         eos_hat = parameters_hats[:, :, 0:1]
-        pi_hat, mu1, mu2, sigma1_hat, sigma2_hat, rho_hat = torch.chunk(parameters_hats[:, :, 1:], 6, dim=2)
-        eos = F.sigmoid(eos_hat)
+        pi_hat, mu1_hat, mu2_hat, sigma1_hat, sigma2_hat, rho_hat = torch.chunk(parameters_hats[:, :, 1:], 6, dim=2)
+        eos = F.sigmoid(-eos_hat)
         pi = F.softmax(pi_hat, dim=2)
+        mu1 = mu1_hat
+        mu2 = mu2_hat
         sigma1 = torch.exp(sigma1_hat)
         sigma2 = torch.exp(sigma2_hat)
         rho = F.tanh(rho_hat)