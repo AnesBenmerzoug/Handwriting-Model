Corrected an error in the loss computation

src/loss.py
@@ -2,6 +2,7 @@ from __future__ import print_function, division
 import torch
 from torch.nn import Module
 import numpy as np
+from functools import reduce
 
 
 class HandwritingLoss(Module):
@@ -11,14 +12,13 @@ class HandwritingLoss(Module):
 
     def forward(self, mdn_parameters, stroke):
         eos, pi, mu1, mu2, sigma1, sigma2, rho = mdn_parameters
-        x_data = stroke[0, 0, 0]
-        y_data = stroke[0, 0, 1]
-        eos_data = stroke[0, 0, 2]
+        x_data, y_data, eos_data = stroke.chunk(3, dim=2)
         N = self.bivariateGaussian(x_data, y_data, mu1, mu2, sigma1, sigma2, rho)
-        Pr = torch.sum(pi * N)
-        loss = - torch.log(Pr + 1e-20) \
-               - torch.log(eos * eos_data + (1.0 - eos) * (1.0 - eos_data))
-        return loss.view(1)
+        term1 = -((pi * N).sum(dim=2, keepdim=True) + 1e-20).log()
+        term2 = -(eos * eos_data + (1.0 - eos) * (1.0 - eos_data)).log()
+        loss = term1 + term2
+        reduction_factor = reduce((lambda x, y: x * y), loss.size())
+        return loss.sum() / reduction_factor
 
     def bivariateGaussian(self, x, y, mu1, mu2, sigma1, sigma2, rho):
         Z = ((x - mu1) / sigma1) ** 2.0 \

src/modules.py
@@ -16,7 +16,9 @@ class GaussianWindow(Module):
         alpha, beta, kappa = abk.chunk(3, dim=2)
         if prev_kappa is not None:
             kappa = kappa + prev_kappa
-        u = torch.autograd.Variable(torch.arange(0, onehot.size(1)).view(1, -1, 1).expand(onehot.size(0), -1, kappa.size(2)))
+        u = torch.autograd.Variable(torch.arange(0, onehot.size(1)))
+        u = u.view(1, -1, 1)
+        u = u.expand(onehot.size(0), -1, kappa.size(2))
         phi = torch.sum(alpha * torch.exp(-beta * ((kappa - u) ** 2)), dim=2).unsqueeze(1)
         window = torch.matmul(phi, onehot)
         return window, kappa

src/trainer.py
@@ -94,6 +94,7 @@ class Trainer(object):
             self.model.reset_state()
             snapshot_loss = None
             for idx in range(strokes.size(1) - 1):
+                self.snapshot_model.detach_state()
                 output = self.model(strokes[:, idx:idx + 1, :], onehot)
                 # Loss Computation
                 if snapshot_loss is None:
@@ -140,7 +141,7 @@ class Trainer(object):
     def train_epoch(self):
         losses = 0.0
         for batch_index, (data) in enumerate(self.trainloader, 1):
-            if batch_index % 50 == 0:
+            if batch_index % 1 == 0:
                 print("Step {}".format(batch_index))
                 print("Average Loss so far: {}".format(losses / batch_index))
             # Split data tuple
@@ -154,41 +155,35 @@ class Trainer(object):
             loss = None
             snapshot_loss = None
             for idx in range(strokes.size(1)-1):
+                self.model.detach_state()
                 output = self.model(strokes[:, idx:idx+1, :], onehot)
                 # Loss Computation
-                if loss is None:
-                    loss = self.criterion(output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
-                else:
-                    loss = loss + self.criterion(output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
+                loss = self.criterion(output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
                 if self.params.optimizer == 'SVRG':
                     # Snapshot Model Forward Backward
                     snapshot_output = self.snapshot_model(strokes[:, idx:idx+1, :], onehot)
-                    if snapshot_loss is None:
-                        snapshot_loss = self.criterion(snapshot_output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
-                    else:
-                        snapshot_loss = snapshot_loss \
-                                        + self.criterion(snapshot_output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
-            inf = float("inf")
-            if loss.data[0] == inf or loss.data[0] == -inf:
-                print("Warning, received inf loss. Skipping it")
-            elif loss.data[0] != loss.data[0]:
-                print("Warning, received NaN loss.")
-            else:
-                losses = losses + loss.data[0]
-            # Zero the optimizer gradient
-            self.optimizer.zero_grad()
-            # Backward step
-            loss.backward()
-            # Clip gradients
-            clip_grad_norm(self.model.parameters(), self.params.max_norm)
-            if self.params.optimizer == 'SVRG':
-                self.snapshot_model.zero_grad()
-                snapshot_loss.backward()
-                clip_grad_norm(self.snapshot_model.parameters(), self.params.max_norm)
-            # Weight Update
-            self.optimizer.step()
-            if self.useGPU is True:
-                torch.cuda.synchronize()
+                    snapshot_loss = self.criterion(snapshot_output, strokes[:, idx+1:idx+2, :]) / strokes.size(1)
+                inf = float("inf")
+                if loss.data[0] == inf or loss.data[0] == -inf:
+                    print("Warning, received inf loss. Skipping it")
+                elif loss.data[0] != loss.data[0]:
+                    print("Warning, received NaN loss.")
+                else:
+                    losses = losses + loss.data[0]
+                # Zero the optimizer gradient
+                self.optimizer.zero_grad()
+                # Backward step
+                loss.backward()
+                # Clip gradients
+                clip_grad_norm(self.model.parameters(), self.params.max_norm)
+                if self.params.optimizer == 'SVRG':
+                    self.snapshot_model.zero_grad()
+                    snapshot_loss.backward()
+                    clip_grad_norm(self.snapshot_model.parameters(), self.params.max_norm)
+                # Weight Update
+                self.optimizer.step()
+                if self.useGPU is True:
+                    torch.cuda.synchronize()
             del onehot, strokes, data
         # Compute the average loss for this epoch
         avg_loss = losses / len(self.trainloader)